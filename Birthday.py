60105b4d2b11c9e1576223169088bed302427df1
Maksym54 <scvrty33@gmail.com>
2023-08-09 13:49:43 +0300

    Fixed

--- Birthday.py
+++ Birthday.py
@@ -16,22 +16,34 @@ def get_birthdays_per_week(users):
     start_of_week = current_date - timedelta(days=current_weekday)
     end_of_week = start_of_week + timedelta(days=6)
 
+    birthday_dict = {}
+
     for user in users:
         birthday = user['birthday']
+        birthday = birthday.replace(year=current_date.year)
+
         if start_of_week <= birthday <= end_of_week:
             day_index = birthday.weekday()
+            day_name = days_of_week[day_index]
             if day_index > 4:
                 day_index = 0
-            day_name = days_of_week[day_index]
+
+            if birthday.date() not in birthday_dict:
+                birthday_dict[birthday.date()] = [user['name']]
+            else:
+                birthday_dict[birthday.date()].append(user['name'])
+
             print(f"{day_name}: {user['name']}")
 
+    print(birthday_dict)
+
 test_users = [
-    {'name': 'Bill', 'birthday': datetime(2023, 8, 8)},
-    {'name': 'Tom', 'birthday': datetime(2023, 8, 7)},
-    {'name': 'Jill', 'birthday': datetime(2023, 8, 9)},
-    {'name': 'Kim', 'birthday': datetime(2023, 8, 10)},
-    {'name': 'Jan', 'birthday': datetime(2023, 8, 12)},
-    {'name': 'Saul', 'birthday': datetime(2023, 8, 13)}
+    {'name': 'Bill', 'birthday': datetime(1998, 8, 8)},
+    {'name': 'Tom', 'birthday': datetime(1998, 8, 7)},
+    {'name': 'Jill', 'birthday': datetime(1997, 8, 9)},
+    {'name': 'Kim', 'birthday': datetime(1999, 8, 10)},
+    {'name': 'Jan', 'birthday': datetime(1996, 8, 12)},
+    {'name': 'Saul', 'birthday': datetime(1998, 8, 13)}
 ]
 
 get_birthdays_per_week(test_users)
